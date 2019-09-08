b486dcc3d5e03a3e20147c640b73655a05f5157b
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-09-08 07:20:55 -0700

    Add files via upload
    This program is solely for the purpose of understanding how the language operates under certain circumstances

---
+++ Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myName As String
+        myName = ""
+
+        Dim myPer As Single
+        myPer = 0
+
+        Dim Counter As Integer
+        Counter = 0
+
+        Console.Write("Enter your name: ")
+        myName = Console.ReadLine()
+
+        For Counter = 1 To 5
+            Console.WriteLine(myName)
+        Next
+
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey()
+    End Sub
+
+End Module
